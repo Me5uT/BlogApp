10b762d8b66f319881caf22085cd3600b4c6b334
Me5uT <me5ut@windowslive.com>
2023-05-17 16:21:14 +0300

    Added Splash Screen & App Logo

--- src/route/StackNavigator.tsx
+++ src/route/StackNavigator.tsx
@@ -1,12 +1,21 @@
 /* eslint-disable react/react-in-jsx-scope */
 import {createStackNavigator} from '@react-navigation/stack';
 import {Posts} from '../pages/posts/Posts';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {PostDetails} from '../pages/post-detail/PostDetails';
+import useData from '../context/useData';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
+  const {loading} = useData();
+
+  useEffect(() => {
+    if (!loading) SplashScreen.hide();
+  }, [loading]);
+
+  console.log('loading', loading);
   return (
     <Stack.Navigator
       initialRouteName="Posts"
